675cc8f76707ba6bc70893465bbe102b85f360f0
Faithe Olson <faithe.olson@gmail.com>
2018-01-30 15:28:36 -0500

    Added validation and try catch

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Lab8
@@ -10,38 +11,87 @@ namespace Lab8
     {
         static void Main(string[] args)
         {
-            //How many players
-           Console.WriteLine("Please enter the number of players:");
-           int NumPlayersRow = int.Parse(Console.ReadLine());
-           int[][] PlayerStats = new int [NumPlayersRow][];
-
-            for (int Row = 0; Row < NumPlayersRow; Row++)
+            try
             {
-                Console.WriteLine($"Enter Player #{Row + 1} times at bat:");
-                int TimesAtBatColumn = int.Parse(Console.ReadLine());
-                PlayerStats[Row] = new int[TimesAtBatColumn];
-                int AtBat = TimesAtBatColumn;
-                int SluggingScoreCount = 0;
-                int NonZeroCount = 0;
-                for (int Column = 0; Column < TimesAtBatColumn; Column++)
+            bool repeat = true;
+            while (repeat == true)
+            {
+                Console.WriteLine("Please enter the number of players:");
+                int NumPlayersRow = int.Parse(ValidatePlayerInput(Console.ReadLine()));
+                int[][] PlayerStats = new int[NumPlayersRow][];
+
+                for (int Row = 0; Row < NumPlayersRow; Row++)
                 {
-                    Console.WriteLine($"Player #{Row +1} result for at-bat #{Column +1} (0-4):");
-                    int SluggingScore = int.Parse(Console.ReadLine());
-                    PlayerStats[Row][Column] = SluggingScore;
-                    SluggingScoreCount += SluggingScore;
-                    if (SluggingScore > 0)
+                    Console.WriteLine($"Player #{Row + 1}: Enter times at bat:");
+                    int TimesAtBatColumn = int.Parse(ValidateAtBatInput(Console.ReadLine()));
+                    PlayerStats[Row] = new int[TimesAtBatColumn]; //***Remember to create new array
+                    int AtBat = TimesAtBatColumn;
+                    int SluggingScoreCount = 0;
+                    int NonZeroCount = 0;
+                    for (int Column = 0; Column < TimesAtBatColumn; Column++)
                     {
-                        NonZeroCount++;
+                        Console.WriteLine($"Player #{Row + 1}: Result for at-bat #{Column + 1} (0-4):");
+                        int SluggingScore = int.Parse(ValidateScores(Console.ReadLine()));
+                        PlayerStats[Row][Column] = SluggingScore;
+                        SluggingScoreCount += SluggingScore;
+                        if (SluggingScore > 0)
+                        {
+                            NonZeroCount++;
+                        }
                     }
+                    Console.WriteLine($"Player #{Row + 1} batting average is:{CalculateBattingAverage(NonZeroCount, AtBat)}");
+                    Console.WriteLine($"Player #{Row + 1} Slugging Percentage is:{CalculateSluggingPercentage(SluggingScoreCount, AtBat)}");
                 }
-                Console.WriteLine($"Player #{Row} batting average is:{CalculateBattingAverage(NonZeroCount,AtBat)}");
-                Console.WriteLine($"Player # {Row} Slugging Percentage is:{CalculateSluggingPercentage(SluggingScoreCount, AtBat)}");
+                Console.WriteLine("Run program again? (Y/N)");
+                repeat = RepeatProgram(Console.ReadLine().ToUpper());
+            }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Whoops! Something went wrong! Let's try again");
             }
 
-            //How many at bats
-            //What were the scores?
-            //calculate slugging percentage
-            //calculate batting average
+        }
+        public static bool RepeatProgram(string Input)
+        {
+            if (Input != "Y")
+            {
+                Console.Clear();
+                Console.WriteLine("Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.Clear();
+                return true;
+            }
+        }
+        public static string ValidateScores(string Input)
+        {
+            while (!Regex.IsMatch(Input, @"(1|2|3|4)"))
+            {
+                Console.WriteLine("Please enter a valid At-Bat number!");
+                Input = Console.ReadLine();
+            }
+            return Input;
+        }
+        public static string ValidateAtBatInput(string Input)
+        {
+            while (!Regex.IsMatch(Input,(@"(\d)")))
+            {
+                Console.WriteLine("Please enter a valid number of times at bat!");
+                Input = Console.ReadLine();
+            }
+            return Input;
+        }
+        public static string ValidatePlayerInput(string Input)
+        {
+            while (!Regex.IsMatch(Input, @"(\d)"))
+            {
+                Console.WriteLine("Please enter a vaild number of players!");
+                Input = Console.ReadLine();
+            }
+            return Input;
         }
         public static double CalculateBattingAverage(int NonZeros, int AtBats)
         {
@@ -49,7 +99,6 @@ namespace Lab8
 
             return BattingAverage;
         }
-
         public static double CalculateSluggingPercentage(int SluggingScoreCount, int AtBats)
         {
             double SluggingPercentage = SluggingScoreCount / (double)AtBats;
